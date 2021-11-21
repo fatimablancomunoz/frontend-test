84c4deaa741f2fd7cedb9da14351e93936d712f9
Fátima Blanco Muñoz <fatima.blanco@kairosds.com>
2021-11-21 23:17:55 +0100

    feat: product list page

---
+++ src/pages/ProductListPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListItem from '../components/listItem/ListItem'
+
+export const ProductListPage = () => {
+  return (
+    <div>
+      <ListItem />
+    </div>
+  )
+}
+
+export default ProductListPage
