0fbf36dc47cc5c7ae8fe2ca5eb1e71bfea7f9396
Fátima Blanco Muñoz <fatima.blanco@kairosds.com>
2021-11-21 22:59:29 +0100

    feat: create postItem service

---
+++ src/services/postIemCart.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export const postItemCart = async (id, colorCode, storageCode) => {
+  const url = 'https://front-test-api.herokuapp.com/api/cart'
+  const body = {
+    id,
+    colorCode,
+    storageCode
+  }
+
+  axios({
+    method: 'post',
+    url: url,
+    data: body
+  })
+
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+export default postItemCart
